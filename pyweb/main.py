96770528e94bbfdc64a516b06212fc628b1f3e89
811417477@qq.com <811417477@qq.com>
2018-12-08 18:07:25 +0800

    huang
    1.提交图片排版
    2.添加时间的处理工具类

--- pyweb/main.py
+++ pyweb/main.py
@@ -1,13 +1,16 @@
 
 #!/usr/bin/evn python
 # coding=utf-8
-
+# 系统或三方库
 from bottle import default_app, get, run
 from bottle import static_file
 from beaker.middleware import SessionMiddleware
-from addMsg import loadMsg
 import random
 import time
+
+# 自写模块
+from addMsg import loadMsg
+from tool.time_tool import time_tool
 
 # 公司地址 http://192.168.4.65:9090/getDogFood
 # 神舟地址 http://192.168.239.1:9090/getDogFood
@@ -25,12 +28,12 @@ session_opts = {
 @get('/image/sisi_self/<filename>')
 def server_static(filename):
     print(filename)
-    return static_file(filename, root='D:/vs/python/pyweb/image/sisi_self')
+    return static_file(filename, root='D:/git/excel/pyweb/pyweb/image/sisi_self')
 
 @get('/getDogFood')
 def callback():
     # 先写入开头
-    ret = '<!DOCTYPE html><html><head><meta charset="utf-8"><title>1</title></head><body>'
+    ret = '<!DOCTYPE html><html><head><meta charset="utf-8"><title>来自仙女和仙男的仙粮</title></head><body>'
 
     # 添加内容
     # 随机抽取一个时间段的语句
@@ -50,11 +53,11 @@ def callback():
 
     # 写入认识的日期
     ret += '<h3>相识于2018年11月8日21点52分</h3>'
+
     # 写入已经认识的时间
     # 2018/11/8 21:52:0 = 1541685120
-    now_time = time.time()
-    pass_time = time.localtime(now_time - 1541685120)
-    ret += "<h3>已相识%s年%s月%s日%s时%s分%s秒<h3>"%(pass_time.tm_year - 1970, pass_time.tm_mon - 1, pass_time.tm_mday, pass_time.tm_hour, pass_time.tm_min, pass_time.tm_sec)
+    pass_time = time_tool.pass_time(1541685120, time.time())
+    ret += "<h3>已相识%s年%s月%s日%s时%s分%s秒<h3>"%(pass_time.tm_year, pass_time.tm_mon, pass_time.tm_mday, pass_time.tm_hour, pass_time.tm_min, pass_time.tm_sec)
 
     # 加上一些空行
     ret += "<br />"
@@ -69,14 +72,25 @@ def callback():
     ret += "<br />"
 
     # 献上仙女的八十二变
-    ret += "<p>" + "她平时的样子" + "</p>"
-    ret += "<p><img src='/image/sisi_self/who.png' alt='who' width='100' height='100'</p>"
-    ret += "<p>" + "当你问她是谁时" + "</p>"
-    ret += "<p><img src='/image/sisi_self/tell.png' alt='tell' width='100' height='100'</p>"
-    ret += "<p>" + "当你要反驳她时" + "</p>"
-    ret += "<p><img src='/image/sisi_self/angry.png' alt='angry' width='100' height='100'</p>"
-    ret += "<p>" + "当她撒娇时" + "</p>"
-    ret += "<p><img src='/image/sisi_self/coquetry.gif' alt='coquetry' width='100' height='100'</p>"
+    # 格式化布局
+    # 声明table
+    ret += '<table width="600" border="0">'
+    ret += '<tr> <td colspan="5" style="background-color:#F8F8FF;"> <h3>仙女的八十二变</h3> </td> </tr>'
+    # 第一行
+    ret += '<tr>'
+    ret += '<td style="background-color:#FFB6C1;width:150px;vertical-align:top;">'
+    ret += '<b>她平时的样子</b> <br> <img src="/image/sisi_self/who.png" alt="who" width="150" height="150"> <br> </td>'
+    ret += '<td style="background-color:#FFB6C1;width:150px;vertical-align:top;">'
+    ret += '<b>当你问她是谁时</b> <br> <img src="/image/sisi_self/tell.png" alt="tell" width="150" height="150"> <br> </td>'
+    ret += '<td style="background-color:#FFB6C1;width:150px;vertical-align:top;">'
+    ret += '<b>当你要反驳她时</b> <br> <img src="/image/sisi_self/angry.png" alt="angry" width="150" height="150"> <br> </td>'
+    ret += '<td style="background-color:#FFB6C1;width:150px;vertical-align:top;">'
+    ret += '<b>当她撒娇时</b> <br> <img src="/image/sisi_self/coquetry.gif" alt="coquetry" width="150" height="150"> <br> </td>'
+    ret += '</tr>'
+    # 结束table
+    ret += '</table>'
+
+    # 结束html
     ret += "</body></html>"
 
     return ret
---
+++ pyweb/tool/time_tool.py
@@ -0,0 +1,72 @@
+import time
+import json
+# 这个系一个时间工具，用来计算技能时间
+
+# 时间对象
+class timr_dbj():
+    tm_year = 0
+    tm_mon = 0
+    tm_mday = 0
+    tm_hour = 0
+    tm_min = 0
+    tm_sec = 0
+
+# 这个系一个时间工具，用来计算技能时间
+class Time_tool():
+
+    # 计算两个时间的时间差
+    # time1 : 时间戳
+    # time2 : 时间戳
+    def pass_time(self, time_1, time_2):
+        # 要返回的时间对象
+        pass_time = timr_dbj()
+        max_time = time_1
+        min_time = time_2
+        # 区分大小顺序
+        if max_time < min_time:
+            tmp = max_time
+            max_time = min_time
+            min_time = tmp
+
+        # 转换时间结构
+        max_time = time.localtime(max_time)
+        min_time = time.localtime(min_time)
+
+        # 开始计算
+        # 先都取他们的时间差
+        pass_time.tm_year = max_time.tm_year - min_time.tm_year
+        pass_time.tm_mon = max_time.tm_mon - min_time.tm_mon
+        pass_time.tm_mday = max_time.tm_mday - min_time.tm_mday
+        pass_time.tm_hour = max_time.tm_hour - min_time.tm_hour
+        pass_time.tm_min = max_time.tm_min - min_time.tm_min
+        pass_time.tm_sec = max_time.tm_sec - min_time.tm_sec
+        # 计算处理负数情况
+        # 秒
+        if pass_time.tm_sec < 0:
+            pass_time.tm_min -= 1
+            pass_time.tm_sec += 60
+        # 分
+        if pass_time.tm_min < 0:
+            pass_time.tm_hour -= 1
+            pass_time.tm_min += 60
+        # 时
+        if pass_time.tm_hour < 0:
+            pass_time.tm_mday -= 1
+            pass_time.tm_hour += 24
+        # 日
+        if pass_time.tm_mday < 0:
+            pass_time.tm_mon -= 1
+            pass_time.tm_mday += 30
+        # 月
+        if pass_time.tm_mon < 0:
+            pass_time.tm_year -= 1
+            pass_time.tm_mon += 12
+        # # 年 这个哪里还用的到
+        # if pass_time.tm_min < 0:
+        #     pass_time.tm_hour -= 1
+        #     pass_time.tm_min += 60
+
+
+        return pass_time
+
+time_tool = Time_tool()
