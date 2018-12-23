53c61dae0109bdf184dd78e0ca3671bb748d1cf4
811417477@qq.com <811417477@qq.com>
2018-12-24 00:03:20 +0800

    huang
    添加回忆主页
    修改路由分类

--- pyweb/main.py
+++ pyweb/main.py
@@ -5,15 +5,12 @@
 from bottle import default_app, get, run
 from bottle import static_file
 from beaker.middleware import SessionMiddleware
-import random
-import time
 
 # 自写模块
-from addMsg import loadMsg
-from tool.time_tool import time_tool
+from getHtml import getHtml
 
 # 公司地址 http://192.168.4.65:9090/getDogFood
-# 神舟地址 http://192.168.239.1:9090/getDogFood
+# 神舟地址 http://192.168.235.1:9090/getDogFood
 # 公网地址 http://118.126.112.214:9090/getDogFood
 
 # 设置session参数
@@ -25,77 +22,31 @@ session_opts = {
 }
 
 # 获取静态文件
+# 目前没办法做到跨文件
 @get('/image/sisi_self/<filename>')
 def server_static(filename):
     print(filename)
-    return static_file(filename, root='D:/git/excel/pyweb/pyweb/image/sisi_self')
+    return static_file(filename, root='./image/sisi_self')
+@get('/image/memory/<filename>')
+def server_memory(filename):
+    print(filename)
+    return static_file(filename, root='./image/memory')
 
+
+# 获取网页部分
+# 获取网站主页
 @get('/getDogFood')
 def callback():
-    # 先写入开头
-    ret = '<!DOCTYPE html><html><head><meta charset="utf-8"><title>来自仙女和仙男的仙粮</title></head><body>'
+    return getHtml.getDogFood()
 
-    # 添加内容
-    # 随机抽取一个时间段的语句
-    rand_num = random.randrange(loadMsg.msg_count) + 1
-    msg_data = loadMsg.msg_data["_" + str(rand_num)]
-
-    # 写入时间
-    ret += "<h3> %s </h3>"%(msg_data["time"])
-
-    # 写入对话
-    dialogue_list = msg_data["dialogue"].split("|")
-    for dialogue in dialogue_list:
-        ret += "<p> %s </p>"%(dialogue)
-
-    # 加上一些空行
-    ret += "<br />"
-
-    # 写入认识的日期
-    ret += '<h3>相识于2018年11月8日21点52分</h3>'
-
-    # 写入已经认识的时间
-    # 2018/11/8 21:52:0 = 1541685120
-    pass_time = time_tool.pass_time(1541685120, time.time())
-    ret += "<h3>已相识%s年%s月%s日%s时%s分%s秒<h3>"%(pass_time.tm_year, pass_time.tm_mon, pass_time.tm_mday, pass_time.tm_hour, pass_time.tm_min, pass_time.tm_sec)
-
-    # 加上一些空行
-    ret += "<br />"
-
-    # 我们间的第一次
-    ret += "<h3>我们间的第一次<h3>"
-    for event in loadMsg.one_data:
-        ret += "<p>" + loadMsg.one_data[event]["time"] + "</p>"
-        ret += "<p>" + loadMsg.one_data[event]["event"] + "</p>"
-
-    # 加上一些空行
-    ret += "<br />"
-
-    # 献上仙女的八十二变
-    # 格式化布局
-    # 声明table
-    ret += '<table width="600" border="0">'
-    ret += '<tr> <td colspan="5" style="background-color:#F8F8FF;"> <h3>仙女的八十二变</h3> </td> </tr>'
-    # 第一行
-    ret += '<tr>'
-    ret += '<td style="background-color:#FFB6C1;width:150px;vertical-align:top;">'
-    ret += '<b>她平时的样子</b> <br> <img src="/image/sisi_self/who.png" alt="who" width="150" height="150"> <br> </td>'
-    ret += '<td style="background-color:#FFB6C1;width:150px;vertical-align:top;">'
-    ret += '<b>当你问她是谁时</b> <br> <img src="/image/sisi_self/tell.png" alt="tell" width="150" height="150"> <br> </td>'
-    ret += '<td style="background-color:#FFB6C1;width:150px;vertical-align:top;">'
-    ret += '<b>当你要反驳她时</b> <br> <img src="/image/sisi_self/angry.png" alt="angry" width="150" height="150"> <br> </td>'
-    ret += '<td style="background-color:#FFB6C1;width:150px;vertical-align:top;">'
-    ret += '<b>当她撒娇时</b> <br> <img src="/image/sisi_self/coquetry.gif" alt="coquetry" width="150" height="150"> <br> </td>'
-    ret += '</tr>'
-    # 结束table
-    ret += '</table>'
-
-    # 结束html
-    ret += "</body></html>"
-
-    return ret
+# 获取我们间的回忆
+@get('/getWeMemory')
+def callback():
+    return getHtml.getWeMemory()
 
 # 函数主入口
 if __name__ == '__main__':
     app_argv = SessionMiddleware(default_app(), session_opts)
     run(app=app_argv, host='0.0.0.0', port=9090, debug=True, reloader=True)
+    print(getHtml.myName)
+    print(getHtml.myAddr)
